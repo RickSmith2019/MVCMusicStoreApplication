132410d5933ba8f5609c8f30aaffef4edfe5c3da
RickSmith2019 <S01159091@acad.tri-c.edu>
2019-09-22 22:15:03 -0400

    Music Store Application

--- MVCMusicStoreApplication/Global.asax.cs
+++ MVCMusicStoreApplication/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using MVCMusicStoreApplication.Models;
+
 
 namespace MVCMusicStoreApplication
 {
@@ -12,6 +15,8 @@ namespace MVCMusicStoreApplication
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new SampleData());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
